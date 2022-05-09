34c7a4c1b23befedbdb35d55e7ead045c8e088d7
Henrique Marques <hmdev132@gmail.com>
2022-05-09 15:02:12 -0300

    first commit

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "309fc17d1df002",
+    pass: "feeb2fb665e859",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Henrique Marques <henrique@hmdev.com.br>",
+      subject,
+      html: body,
+    });
+  }
+}
